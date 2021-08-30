a4d34f2c5f10b9af5774f195c61a9cc582a37a83
Yue Hu <sammo3182@sina.com>
2021-08-30 15:46:32 +0800

    moving the images to the cn folder

--- R/drhur.R
+++ R/drhur.R
@@ -2,13 +2,14 @@
 #'
 #' The function calls a \code{\link[learnr]{run_tutorial}} function to open a \code{learnr} project. The project list is continually updated along with the R workshop with Dr. Hu.
 #' @param workshop A character vector indicating the name of the workshop to open. See details for the workshop list.
+#' @param language A character vector indicating the display language. The current version includes workshops in English (\code(en)) and simple Chinese (\code(cn)). The default is Chinese
 #'
-#' @details The list of workshop available so far:
+#' @details The list of workshop available for the current version:
 #' \itemize{
-#' \item Playing around with R (\code{playing}): Intro to basic data structure in R; inputting, outputting, and creating data.
-#' \item Data munging (\code{munging}): Generalizing (such as sorting and summarizing) and extracting (selecting or adjusting certain columns or rows) data frames.
-#' \item Data analyzing (\code{analyzing}): Analyze data with descriptive and multivariate tools and diagnoses.
-#' \item Data presenting (\code{visualizing}): Present descriptive and analytic results with `ggplot2`
+#' \item \code{playing}: Intro to basic data structure in R; inputting, outputting, and creating data (English and Chinese).
+#' \item \code{munging}: Generalizing (such as sorting and summarizing) and extracting (selecting or adjusting certain columns or rows) data frames (English and Chinese).
+#' \item \code{analyzing}: Analyze data with descriptive and multivariate tools and diagnoses (English and Chinese).
+#' \item Data presenting (\code{visualizing}): Present descriptive and analytic results with `ggplot2` (English and Chinese).
 #' }
 #'
 #' @import learnr
@@ -16,6 +17,7 @@
 #' @export
 
 drhur <-
-  function(workshop) {
+  function(workshop, language = "cn") {
+    if(language == "cn") workshop <- paste0(workshop, "_cn")
     learnr::run_tutorial(workshop, package = "drhur")
   }
