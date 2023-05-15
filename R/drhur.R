c61a262c378c0425cd03922f46968f5ba4b02255
sammo3182 <sammo3182@sina.com>
2023-05-15 10:04:37 +0800

    role revision in description

--- R/drhur.R
+++ R/drhur.R
@@ -3,7 +3,7 @@
 #' The function calls a \code{\link[learnr]{run_tutorial}} function to open a \code{learnr} project. The project list is continually updated along with the R workshop with Dr. Hu.
 #' 
 #' @param workshop A character vector indicating the name of the workshop to open. See details for the workshop list.
-#' @param language A character vector indicating the display language. The current version includes workshops in English (\code{en}) and simple Chinese (\code{cn}). The default is Chinese.
+#' @param lang A character vector indicating the display language. The current version includes workshops in English (\code{en}) and simple Chinese (\code{cn}). The default is Chinese.
 #'
 #' @details The list of workshop available for the current version:
 #' \itemize{
@@ -18,8 +18,8 @@
 #'
 #' @export
 
-drhur <- function(workshop, language = "cn") {
-  lang_suffix <- ifelse(language == "cn", "_cn", "_en")
+drhur <- function(workshop, lang = "cn") {
+  lang_suffix <- ifelse(lang == "cn", "_cn", "_en")
   workshop <- paste0(workshop, lang_suffix)
   learnr::run_tutorial(workshop, package = "drhur")
 }
--- dev/buildingCodes.R
+++ dev/buildingCodes.R
@@ -3,9 +3,11 @@
 devtools::install(dependencies = TRUE, build_vignettes = TRUE)
 
 # In between
-# 1. Updating the NEWS.md 
-# 1. Updating the DESCRIPTION if necessary
+# 1. Update the NEWS.md 
+# 1. Update the DESCRIPTION if necessary
 
-# release to cran
+
+# Release to cran
+# 1. Delete the htmls
 
 devtools::submit_cran() 
